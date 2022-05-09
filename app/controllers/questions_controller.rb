e098fc4d38d7f4c6336a84401a06c58dc42b2e8d
Reina Kitazawa <kreina1015@yahoo.co.jp>
2022-05-09 14:13:08 +0900

    stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if params[:question] == "I am going to work"
+      @question = "Great!"
+    elsif params[:question].end_with?('?')
+      @question = "Silly question, get dressed and go to work!"
+    else
+      @question = "I don't care, get dressed and go to work!"
+    end
+  end
+end
